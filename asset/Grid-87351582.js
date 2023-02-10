0dfd2467e163f42e4c0f8e4a9f57a013288d8fc7
erichfrom2023 <122940324+erichfrom2023@users.noreply.github.com>
2023-02-10 14:32:58 +0600

    Add files via upload

---
+++ asset/Grid-87351582.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-c27b6911.js";import{o as s,c as t,b as e,F as r,r as c}from"./index-6b3f46e6.js";const n={},_={id:"grid"},a={class:"container"},i={class:"row"},d={class:"col-lg-1"};function l(f,m){return s(),t("div",_,[e("div",a,[e("div",i,[(s(),t(r,null,c(12,u=>e("div",d)),64))])])])}const h=o(n,[["render",l]]);export{h as default};
